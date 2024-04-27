f5803c0e7f21a38f4e885978504a0847c109ea71
Souptik18 <souptiksarkar2017@gmail.com>
2024-04-27 20:10:45 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const db = require('./config/mongoose');
+
+const port = 8000;
+const app = express();
+
+// Set up view engine as ejs
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Serve static files from the assets directory
+app.use(express.static('./assets'));
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+// Load express routes
+app.use('/', require('./routes/index'));
+
+// Start the server
+app.listen(port, function (err) {
+  if (err) {
+    console.log('Error Running the Server!');
+    return;
+  }
+  console.log('Server is up and running on port:', port);
+});
